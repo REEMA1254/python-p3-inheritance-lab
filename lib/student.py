df6e8a2a551ffbea67c552c0ac61ae71d8686a82
maryam <Reemaali9048@gmail.com>
2023-12-04 09:20:58 +0300

    complete assigment

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,10 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
+    def _init_(self, first_name, last_name):
+        super()._init_(first_name, last_name)
+        self.knowledge = []
+    
+    def learn(self, lesson):
+        self.knowledge.append(lesson)
         pass
